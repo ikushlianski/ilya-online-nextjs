ce80397542f5dddee38ef453bd38dc4b0f6e97be
Ilya Kushlianski <kushliansky@gmail.com>
2020-01-17 01:11:32 +0300

    Add routing

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
 
 import { Navbar, NavbarItem } from '../Navbar';
 
 export const Header = () => {
   return (
     <Navbar>
-      <NavbarItem>Home</NavbarItem>
-      <NavbarItem>Portfolio</NavbarItem>
-      <NavbarItem>Skills</NavbarItem>
-      <NavbarItem>About me</NavbarItem>
+      <NavbarItem>
+        <Link href="/">Home</Link>
+      </NavbarItem>
+      <NavbarItem>
+        <Link href="/works">Portfolio</Link>
+      </NavbarItem>
+      <NavbarItem>
+        <Link href="/skills">Skills</Link>
+      </NavbarItem>
+      <NavbarItem>
+        <Link href="/about">About me</Link>
+      </NavbarItem>
     </Navbar>
   );
 };
