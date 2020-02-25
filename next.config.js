19f2da83e7cc3a1cb935fb4c2ff73bf3e8d83df1
Ilya Kushlianski <kushliansky@gmail.com>
2020-02-26 00:06:56 +0300

    Add env variables support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
+const path = require('path');
+
 const withSass = require('@zeit/next-sass');
 const withCss = require('@zeit/next-css');
+
+require('dotenv').config({
+  path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`),
+});
 
 module.exports = withCss(
   withSass({
     webpack(config, options) {
       return config;
+    },
+    env: {
+      API_URL: process.env.API_URL,
     },
   }),
 );
