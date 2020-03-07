89c747ad76e0afa04398524be9693e8ca75b142f
Ilya Kushlianski <kushliansky@gmail.com>
2020-03-07 22:50:16 +0300

    Make Toggle component more flexible

--- src/ui-kit/Toggle/Toggle.js
+++ src/ui-kit/Toggle/Toggle.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 import './Toggle.scss';
 
-export const Toggle = ({ onChange, value }) => (
+export const Toggle = ({
+  onChange,
+  checkedColor,
+  uncheckedColor,
+  name,
+  value = {},
+  checked = false,
+}) => (
   <div className="Toggle">
     <label className="switch">
-      <input type="checkbox" onChange={onChange} checked={value} />
-      <span className="slider" />
+      <input
+        type="checkbox"
+        onChange={onChange}
+        value={value}
+        checked={checked}
+        name={name}
+      />
+      <span
+        className="slider"
+        style={{ backgroundColor: checked ? checkedColor : uncheckedColor }}
+      />
     </label>
   </div>
 );
