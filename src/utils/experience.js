cbc9710c2384f3dc4c7aa852a6ba7afbd81f54e0
Ilya Kushlianski <kushliansky@gmail.com>
2020-03-12 01:43:49 +0300

    Set up Jest, cover experience counter with tests

--- src/utils/experience.js
+++ src/utils/experience.js
@@ -1,10 +1,14 @@
 const MONTH = 2.628e9;
 
 export const getTotalExperience = (jobs = []) => {
+  if (jobs.length === 0) {
+    return 'No experience';
+  }
+
   const totalMonths = jobs.reduce((acc, cur) => {
     const end = Date.parse(cur.end) || Date.now();
     const start = Date.parse(cur.start);
-    acc += Math.ceil((end - start) / MONTH);
+    acc += Math.round((end - start) / MONTH);
 
     return acc;
   }, 0);
@@ -12,5 +16,7 @@ export const getTotalExperience = (jobs = []) => {
   const wholeYears = Math.floor(totalMonths / 12);
   const months = totalMonths % 12;
 
-  return `${wholeYears} years ${months} months`;
+  return `${wholeYears ? wholeYears + ' years' : ''}${
+    wholeYears && months ? ' ' : ''
+  }${months ? months + ' month' + (months === 1 ? '' : 's') : ''}`;
 };
---
+++ src/utils/experience.test.js
@@ -0,0 +1,56 @@
+import dayjs from 'dayjs';
+
+import { getTotalExperience } from './experience';
+
+test('handles empty values', () => {
+  expect(getTotalExperience()).toBe('No experience');
+});
+
+test("prints month if there's 1 month", () => {
+  expect(
+    getTotalExperience([
+      {
+        start: minskTime(dayjs().startOf('month')),
+        end: minskTime(dayjs().endOf('month')),
+      },
+    ]),
+  ).toBe('1 month');
+});
+
+test('prints only months if less than 1 year', () => {
+  expect(
+    getTotalExperience([
+      {
+        start: minskTime(dayjs().subtract(2, 'month')),
+        end: minskTime(dayjs().startOf('month')),
+      },
+    ]),
+  ).toBe('2 months');
+});
+
+test('counts Klika Experience', () => {
+  expect(
+    getTotalExperience([
+      {
+        start: minskTime(dayjs('4 June 2018')),
+        end: minskTime(dayjs('4 June 2020')),
+      },
+    ]),
+  ).toBe('2 years');
+});
+
+test('counts usual experience with both months and years', () => {
+  expect(
+    getTotalExperience([
+      {
+        start: minskTime(dayjs('4 June 2018')),
+        end: minskTime(dayjs('21 June 2020')),
+      },
+    ]),
+  ).toBe('2 years 1 month');
+});
+
+const minskTime = time =>
+  dayjs(time)
+    .add(3, 'hour')
+    .toDate();
