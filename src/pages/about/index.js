4b724523fecb195267496fd75d3d192c2973348d
Ilya Kushlianski <kushliansky@gmail.com>
2020-03-25 00:13:46 +0300

    Sketch out About Me page, improve ui-kit components

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,12 +1,86 @@
 import React from 'react';
 
-import { Block } from '../../ui-kit/Block';
+import { Block, Card } from '../../ui-kit';
 import { Layout } from '../../components/Layout';
+
+import './AboutPage.scss';
 
 const AboutPage = () => (
   <Layout title="Ilya Kushlianski - About me">
     <div className="AboutMePage">
-      <Block>Hi from About Me page!</Block>
+      <Block className="Block--lighter">
+        <h1 className="AboutMePage__Heading">Coding</h1>
+        <div className="AboutMePage__QA">
+          <Card flat>
+            <h3 className="AboutMePage__Q">Question</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at
+              consectetur eaque facere incidunt libero, quos! Blanditiis
+              corporis dolores eveniet nam numquam quasi recusandae sunt?
+              Blanditiis consequuntur dicta ea inventore laboriosam odio
+              sapiente! Alias deserunt eius facilis quae quo repellat.
+            </p>
+          </Card>
+          <Card flat>
+            <h3 className="AboutMePage__Q">Question 2</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Accusamus architecto consequatur distinctio, doloribus ducimus
+              eaque exercitationem fugit illum in iste labore laudantium maiores
+              maxime nulla odit, officiis omnis perferendis recusandae rem
+              sapiente sed sunt tempore temporibus velit veniam vitae voluptatem
+              voluptates. Adipisci doloribus dolorum, esse, illum iusto porro
+              quaerat quas quidem repudiandae similique tempora vitae!
+            </p>
+          </Card>
+          <Card flat>
+            <h3 className="AboutMePage__Q">Question 3</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at
+              consectetur eaque facere incidunt libero, quos! Blanditiis
+              corporis dolores eveniet nam numquam quasi recusandae sunt?
+              Blanditiis consequuntur dicta ea inventore laboriosam odio
+              sapiente! Alias deserunt eius facilis quae quo repellat.
+            </p>
+          </Card>
+        </div>
+      </Block>
+
+      <Block>
+        <h1 className="AboutMePage__Heading">Personal</h1>
+        <div className="AboutMePage__QA">
+          <Card flat>
+            <h3 className="AboutMePage__Q">Question</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
+              enim fuga iste itaque laboriosam nam neque odio odit optio
+              recusandae, saepe similique sint tempora ullam voluptates!
+              Accusamus esse facere harum ipsam minus omnis placeat quasi
+              recusandae repellendus vitae. Aspernatur deserunt et eveniet
+              facere labore necessitatibus perspiciatis quod sapiente tempore
+              voluptates?
+            </p>
+          </Card>
+        </div>
+      </Block>
+
+      <Block className="Block--darker">
+        <h1 className="AboutMePage__Heading">Career</h1>
+        <div className="AboutMePage__QA">
+          <Card flat>
+            <h3 className="AboutMePage__Q">Question</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
+              enim fuga iste itaque laboriosam nam neque odio odit optio
+              recusandae, saepe similique sint tempora ullam voluptates!
+              Accusamus esse facere harum ipsam minus omnis placeat quasi
+              recusandae repellendus vitae. Aspernatur deserunt et eveniet
+              facere labore necessitatibus perspiciatis quod sapiente tempore
+              voluptates?
+            </p>
+          </Card>
+        </div>
+      </Block>
     </div>
   </Layout>
 );
--- src/ui-kit/Card/Card.js
+++ src/ui-kit/Card/Card.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import cx from 'classnames';
 
 import './Card.scss';
 
-export const Card = ({ children }) => {
-  return <div className="Card">{children}</div>;
+export const Card = ({ children, flat }) => {
+  const cls = cx('Card', {
+    'Card--flat': flat,
+  });
+
+  return <div className={cls}>{children}</div>;
 };
