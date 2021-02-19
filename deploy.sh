0517dc17199bbcaf1d8f6f3f6268e576e344c9cf
Ilya Kushlianski <kushliansky@gmail.com>
2021-02-20 00:39:52 +0300

    Read CloudFront distribution id from env file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+
+DISTRIBUTION_ID=$(grep CF_DISTRIBUTION_ID .env.production | cut -d '=' -f2)
 
 npx next build
 npx next export
@@ -15,5 +18,5 @@ mv out/front-page.html out/index.html
 
 aws s3 rm s3://ilya.online --recursive
 aws s3 sync out s3://ilya.online
-aws cloudfront create-invalidation --distribution-id E1VMLKHJV28HND --paths "/*"
+aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" --paths "/*"
 
