1a6c7caab70e31189adde0f16fa00281fcef421d
Ilya Kushlianski <kushliansky@gmail.com>
2021-02-24 20:58:00 +0300

    Update project preferences

--- src/components/FrontPage/Advantages.js
+++ src/components/FrontPage/Advantages.js
@@ -15,20 +15,19 @@ export const Advantages = () => {
         </p>
         <p className="Advantages__Text">
           <div className="Advantages__Accent">Domain:</div>
-          Any end-user-oriented project, preferably the one that has started or
-          is going to start
+          Any end-user-oriented project, preferably a project from scratch
         </p>
         <p className="Advantages__Text">
           <div className="Advantages__Accent">Tech stack:</div>
-          Preferably a full-stack React and Node project, or a frontend project
+          Preferably a full-stack React and Node project, or a backend project
         </p>
         <p className="Advantages__Text">
           <div className="Advantages__Accent">Communication:</div>
-          Would be nice to communicate with a foreign customer on a daily basis
+          Would be nice to communicate with customers on a daily basis
         </p>
         <p className="Advantages__Text">
-          <div className="Advantages__Accent">Outlook:</div>I want to grow as a
-          developer and, later, take managerial responsibilities
+          <div className="Advantages__Accent">Outlook:</div>I continue growing
+          as a developer. Next step is Team Lead position
         </p>
       </div>
     </div>
