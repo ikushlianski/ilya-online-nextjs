4c08b54ec9a4970a03f8a79f0a5f8bf3b5f7d43a
Ilya Kushlianski <kushliansky@gmail.com>
2021-02-25 23:09:00 +0300

    filter out hidden works in portfolio

--- src/pages/works/index.js
+++ src/pages/works/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import fetch from 'isomorphic-unfetch';
 
-import { Block } from '../../ui-kit/Block';
-import { Layout } from '../../components/Layout';
+import { Block } from '../../ui-kit';
+import { Layout } from '../../components';
 import { keyBy } from '../../utils';
-import { WorksBox } from '../../components/Works';
+import { WorksBox } from '../../components';
 
 import './WorksPage.scss';
 
@@ -18,21 +18,22 @@ const WorksPage = ({ works }) => {
     >
       <div className="WorksPage">
         <div className="WorksPage__Intro">
-          <h1 className="WorksPage__PageTitle">Portfolio</h1>
+          <h1 className="WorksPage__PageTitle">Pet projects</h1>
           <p className="WorksPage__PageDescription">
             It is hard sometimes to carve out time for coding apart from your
             regular job. But that is how you grow as a developer - working on
             your pet projects.
           </p>
           <p>
-            Here they are: from the newest, tiny and bigger ones, to the first,
-            naive projects that mark the beginning of my web dev journey.
+            Here they are: from the newest and bigger ones, to the first, naive
+            projects that mark the beginning of my web dev journey.
           </p>
         </div>
         <Block>
           <div className="WorksPage__Blocks">
-            <WorksBox title="Bigger/latest works" items={intermediate} />
-            <WorksBox title="Smaller/early examples" items={beginner} />
+            <WorksBox title="Advanced code" items={advanced} />
+            <WorksBox title="Intermediate works" items={intermediate} />
+            <WorksBox title="Small examples" items={beginner} />
           </div>
         </Block>
       </div>
@@ -45,7 +46,9 @@ export const getStaticProps = async _ => {
     const res = await fetch(`${process.env.API_URL}/works`);
     const json = await res.json();
 
-    const works = Object.entries(keyBy(json, 'group')).reduce(
+    const withoutHidden = json.filter(item => !item.hidden);
+
+    const works = Object.entries(keyBy(withoutHidden, 'group')).reduce(
       (acc, [groupName, worksGroup]) => {
         acc[groupName] = worksGroup.sort((a, b) => b.date - a.date);
 
