ed7f7ed93bd2df30f1d51ba44954d9f6397f2159
Ilya Kushlianski <kushliansky@gmail.com>
2023-10-04 22:36:23 +0200

    Change h1 and h2 on front page for better semantics
    Signed-off-by: Ilya Kushlianski <kushliansky@gmail.com>

--- src/components/FrontPage/Intro.js
+++ src/components/FrontPage/Intro.js
@@ -19,11 +19,11 @@ export const Intro = () => (
       {/*<img src="/cover-photo-main2-optimized.jpeg" alt="Ilya Kushlianski" className="Intro__ImageDesktop" />*/}
       <div className="Intro__DesktopOverlay shown-md" />
       <div className="Intro__DesktopInfoContainer">
-        <p className="Intro__PrimaryDesktop shown-md">Ilya Kushlianski</p>
-        <p className="Intro__SecondaryDesktop shown-md">
+        <h1 className="Intro__PrimaryDesktop shown-md">Ilya Kushlianski</h1>
+        <h2 className="Intro__SecondaryDesktop shown-md">
           {process.env.MODE === 'fs' ? 'Full-stack Javascript' : 'Front-End'}{' '}
           Developer
-        </p>
+        </h2>
         <div className="Intro__Controls shown-md">
           <Button inverted href={getLinkByAlias('cv')}>
             See my CV
